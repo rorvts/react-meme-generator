1dd2781607301c58a2d49ccf273a80824daf4833
rorvts <rorvts@gmail.com>
2020-07-24 13:08:20 +0200

    Working with components

---
+++ todo/src/components/MyInfo/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MyInfo = () => {
+  return (
+    <div>
+      <h1>Rodrigo</h1>
+      <p>I'm a developer!</p>
+      <ol>
+        <li>Amsterdam</li>
+        <li>Boston</li>
+        <li>Svalbard</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyInfo;
