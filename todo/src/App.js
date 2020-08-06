5aa9c14556c0edd7b7ba21b9ff9053ab1f578a46
rorvts <rorvts@gmail.com>
2020-08-06 18:30:21 +0200

    Modifying state

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import "./styles.css";
-import List from "./components/List/List";
-import api from "./todoData";
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      list: api
+      count: 0
     }
+    this.handleClick = this.handleClick.bind(this);
   }
-  
+
+  handleClick() {
+    this.setState(prevState => {
+      return {count: prevState.count + 1}
+    })
+  }
+
   render(){
-    const data = this.state.list.map(item => <List key={item.id} list={item}/> )
     return (
-      <div className="todo-list">
-        {data}
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Increase</button>
       </div>
     )
   }
 }
-
 
 export default App;
--- todo/src/components/List/List.js
+++ todo/src/components/List/List.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./styles.css";
 
 const List = (props) => {
+  function handleCheckbox(){
+    if(props.list.completed){
+      props.list.completed = false;
+    } else {
+      props.list.completed = true;
+    }
+  }
   return (
       <div className="todo-item">
-        <input type="checkbox" checked={props.list.completed}/>
+        <input type="checkbox" checked={props.list.completed} onChange={handleCheckbox}/>
         <p>{props.list.text}</p>
       </div>
   );
